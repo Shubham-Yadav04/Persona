2b859bb10c20e4b99748b3d8af58b14599790a0e
Shubham-Yadav04 <risuyadav24@gmail.com>
2026-04-30 20:01:53 +0530

    updateing type error

--- persona-frontend/components/ChatInterface.tsx
+++ persona-frontend/components/ChatInterface.tsx
@@ -4,34 +4,32 @@ import { Send, Sparkles, User, Bot } from "lucide-react";
 import { useState, useRef, useEffect } from "react";
 import { cn } from "@/lib/utils";
 import Error from "next/error";
-import axios from "axios";
+
 
 type Message = {
   id: string;
-  role: "user" | "bot";
+  role: "user" | "assistant";
   content: string;
+  loading?: boolean;
 };
 export const ChatInterface = () => {
-  const [messages, setMessages] = useState<Message[]>([
-    {
-      id: "1",
-      role: "bot",
-      content: "Hi there, I am Shubham Yadav 's virtual Persona. How may I assist you today?",
-    },
-  ]);
+  const [messages, setMessages] = useState<Message[]>([]);
   const [sessionId,setSessionId]= useState<string>(crypto.randomUUID());
   const [input, setInput] = useState("");
+  const controllerRef = useRef<AbortController | null>(null);
   const messagesEndRef = useRef<HTMLDivElement>(null);
 const [response,setResponse]=  useState(false);
+
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
-const controller = new AbortController();
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
 
   const handleSend =async () => {
+    controllerRef.current?.abort(); // cancel previous
+controllerRef.current = new AbortController();
     if (response) return;
     if (!input.trim()) return;
 
@@ -40,12 +38,12 @@ const controller = new AbortController();
       role: "user",
       content: input,
     };
-    setMessages((prev) => [...prev, newMessage]);
+    setMessages((prev: Message[]) => [...prev || [], newMessage]);
     setResponse(true);
     const userQuery=input;
 setInput("")
     // Simulate bot response
-    let botMessageId: string | null = null;
+    let botMessageId: string | null;
 try {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URI}`, {
     method: "POST",
@@ -53,13 +51,13 @@ try {
     headers: {
       "Content-Type": "application/json",
     },
-    signal: controller.signal
+   signal: controllerRef.current.signal,
+    cache:"no-cache"
   });
 
   if (!res.ok) throw new Error("Request failed");
 
-  if (!res.body) throw new Error("No response body");
-
+  if (!res.body) throw new Error("No response body"); 
   const reader = res.body.getReader();
   const decoder = new TextDecoder();
 
@@ -67,35 +65,46 @@ try {
   let buffer="";
   // Add empty bot message first
    botMessageId = Date.now().toString();
-  setMessages((prev) => [
-    ...prev,
-    { id: botMessageId, role: "bot", content: "Searching ..." }
-  ]);
+  setMessages(prev => [
+  ...(prev || []),
+  {
+    id: botMessageId,
+    role: "assistant",
+    content: "",
+    loading: true
+  }
+]);
 
   while (true) {
   const { value, done } = await reader.read();
-  if (done) break;
+  if (done){
+    break;
+  } 
 
   buffer += decoder.decode(value, { stream: true });
 
   // Split complete SSE messages
-  const parts = buffer.split("\n\n");
-  buffer = parts.pop(); // keep incomplete part
+  const parts = buffer.split("\n");
+  buffer = parts.pop();
 
   for (const part of parts) {
     if (part.startsWith("data:")) {
-      const data = part.replace("data:", "").trim();
+  const data = part.replace("data:", "").trim();
 
-      fullResponse += data;
+  fullResponse += data;
 
-      setMessages((prev) =>
-        prev.map((msg) =>
-          msg.id === botMessageId
-            ? { ...msg, content: fullResponse }
-            : msg
-        )
-      );
-    }
+  setMessages((prev: Message[]) =>
+    prev?.map(msg =>
+      msg.id === botMessageId
+        ? {
+            ...msg,
+            content: fullResponse,
+            loading: false, // remove searching
+          }
+        : msg
+    )
+  );
+}
   }
 }
 
@@ -105,8 +114,9 @@ try {
  catch (error) {
   console.log(error);
   setResponse(false);
-  setMessages(prev=>
-    prev.map(msg=> msg.id===botMessageId?
+ 
+  setMessages((prev: Message[]) =>
+    prev?.map(msg=> msg.id===botMessageId!?
       {...msg, content:"Sorry, something went wrong. Please try again."}
       :msg
     ) 
@@ -117,7 +127,20 @@ return (
     <div className="flex flex-col h-full max-w-4xl mx-auto p-4 md:p-6 relative z-10">
       <div className=" relative flex-1 overflow-y-auto space-y-6  pb-20 scrollbar-none min-h[100vh]">
         <AnimatePresence initial={false}>
-          {messages.map((message) => (
+        {
+          messages.length===0 && (
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}    
+              transition={{ duration: 0.5, ease: "easeOut" }}
+              className="flex flex-col items-center gap-4 mt-20 text-gray-500"
+            > 
+              <Bot className="w-10 h-10 text-gray-400" />
+              <p className="text-lg">How can i Help You </p>
+            </motion.div>
+          )
+        }
+          {messages?.map((message) => (
             <motion.div
               key={message.id}
               initial={{ opacity: 0, y: 20, scale: 0.95 }}
@@ -131,10 +154,10 @@ return (
               <div
                 className={cn(
                   "w-10 h-10 rounded-full flex items-center justify-center shrink-0 border border-white/10",
-                  message.role === "bot" ? "bg-black/50" : "bg-white/10"
+                  message.role === "assistant" ? "bg-black/50" : "bg-white/10"
                 )}
               >
-                {message.role === "bot" ? (
+                {message.role === "assistant" ? (
                   <Sparkles className="w-5 h-5 text-brown-800" />
                 ) : (
                   <User className="w-5 h-5 text-gray-400" />
@@ -143,12 +166,18 @@ return (
               <div
                 className={cn(
                   "relative max-w-[80%] md:max-w-[70%] p-4 rounded-2xl text-sm md:text-base leading-relaxed backdrop-blur-md ",
-                  message.role === "bot"
+                  message.role === "assistant"
                     ? "text-gray-200"
                     : "bg-white/10 border border-white/20 rounded-tr-none text-white shadow-[0_0_15px_rgba(255,255,255,0.1)]"
                 )}
               >
-                {message.content}
+                {message.loading ? (
+  <span className="animate-pulse text-gray-400">
+    Searching<span className="animate-bounce">...</span>
+  </span>
+) : (
+  message.content
+)}
               
               </div>
             </motion.div>
@@ -171,7 +200,7 @@ return (
           <button
             onClick={handleSend}
             className="absolute right-2 top-1/2 -translate-y-1/2 w-10 h-10 bg-white text-black rounded-full flex items-center justify-center hover:scale-105 hover:bg-gray-200 transition-all disabled:opacity-50 disabled:hover:scale-100"
-            disabled={!input.trim()}
+            disabled={!input.trim()  || response}
           >
             <Send className="w-4 h-4 ml-0.5" />
           </button>
